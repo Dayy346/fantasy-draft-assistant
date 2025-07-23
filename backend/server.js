20dd8df9147f8b193f2fd95d5b7827fb68ec8198
Dayyan Hamid <dayyan6093@gmail.com>
2025-07-22 22:51:08 -0400

    add data transformation

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,30 @@ const results = [];
 fs.createReadStream(filePath)
     .pipe(csv())
     .on("data", (row) => {
-        results.push(row);
+        // results.push(row);
+        const name = row.player;
+        const games = parseFloat(row.games);
+        const totalPoints = parseFloat(row.fantasy_points);
+
+        // adding a error check to skip data if bad
+        if (isNaN(games) || isNaN(totalPoints) || games === 0) return;
+
+        const ppg = totalPoints / games;
+
+        const player = {
+            name,
+            games,
+            total_points: totalPoints,
+            points_per_game: parseFloat(ppg.toFixed(2))
+        };
+
+        results.push(player);
     })
     .on("end",() => {
-        console.log("CSV Parsed");
-        console.log(results);
+        const outputPath = path.join(__dirname, "data", "rb_2024_transformed.json");
+
+        fs.writeFileSync(outputPath, JSON.stringify(results, null, 2));
+        
+        console.log(`saved ${results.length} players to rb_2024_transformed.json`);
+        // console.log(results); to check if the array acc took the players
     })
